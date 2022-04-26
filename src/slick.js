25a1e2b9aafe5f38338638fff654a75266c3d6f3
Hesfra <contato.chfreire@gmail.com>
2022-04-26 20:00:48 -0300

    Curriculo quase pronto

---
+++ src/slick.js
@@ -0,0 +1,9 @@
+$('.slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    prevArrow: 'display:none',
+    nextArrow: 'display:none'
+    
+  });
