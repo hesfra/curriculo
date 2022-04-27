b729872a80ce3f71f258faa90671ae7e8a6bb131
Hesfra <contato.chfreire@gmail.com>
2022-04-27 00:37:35 -0300

    finalizando responsividade

--- src/slick.js
+++ src/slick.js
@@ -4,6 +4,24 @@ $('.slick').slick({
     autoplay: true,
     autoplaySpeed: 3500,
     prevArrow: 'display:none',
-    nextArrow: 'display:none'
-    
-  });
+    nextArrow: 'display:none',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            centerMode:true,
+          }
+        },
+        {
+          breakpoint: 680,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode:true
+          }
+        }
+      ]
+    });
